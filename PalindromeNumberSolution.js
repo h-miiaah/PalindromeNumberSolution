9e79960afaeeec108b98ae774ca7d08e07d6d121
h-miiaah <haim.miiaah5@gmail.com>
2021-12-24 14:59:26 -0500

    explained solution

--- PalindromeNumberSolution.js
+++ PalindromeNumberSolution.js
@@ -5,3 +5,13 @@
  var isPalindrome = function(x) {
     return x == x.toString().split('').reverse().join('');
 };
+
+/*
+
+- Convert the number to a string which will create a character array.
+- Reverse it.
+- Join that array back to a string.
+- Check if both the string match.
+- The function is using == because we want to check if both sides are equal regardless of thier type.
+
+*/
